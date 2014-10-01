f9f6c82411ba1d3789983c3259dfe535bcf8ebf8
Charles Pick <charles.pick@gmail.com>
2014-10-01 23:17:15 +0100

    add preliminary benchmarks

---
+++ bench/map-3.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var Collection = require('../lib'),
+    utils = require('./utils');
+
+var fns = utils.fns('item', 'return item * item + Math.random()');
+
+exports['Array::map()'] = function () {
+  var input = [1,2,3];
+  return input.map(fns());
+};
+
+exports['Collection.map()'] = function () {
+  var input = Collection(1,2,3);
+  return input.map(fns());
+};
